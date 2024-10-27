f45f3f3ce0dd0651fe33f247fc53fcf3ac7ac6af
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2024-10-27 14:26:54 -0500

    Cambios en food y clase gamePanel

--- snakehilos/src/main/java/co/edu/uniquindio/poo/model/Food.java
+++ snakehilos/src/main/java/co/edu/uniquindio/poo/model/Food.java
@@ -1,28 +1,25 @@
 package co.edu.uniquindio.poo.model;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+import static co.edu.uniquindio.poo.model.GamePanel.*;
+
 public class Food {
+    int x, y;
+    Color color;
+    static final int SIZE = 20; // Tamaño de la comida
 
-    import java.awt.Color;
-    import java.awt.Graphics;
-    import java.util.Random;
+    public Food() {
+        Random rand = new Random();
+        this.x = MARGIN_LEFT + rand.nextInt((MARGIN_RIGHT - MARGIN_LEFT) / SIZE) * SIZE;
+        this.y = MARGIN_TOP + rand.nextInt((MARGIN_BOTTOM - MARGIN_TOP) / SIZE) * SIZE;
+        this.color = Color.RED;
+    }
 
-    import static co.edu.uniquindio.sigecim.snake.GamePanel.*;
-
-    public class Food {
-        int x, y;
-        Color color;
-        static final int SIZE = 20; // Tamaño de la comida
-
-        public Food() {
-            Random rand = new Random();
-            this.x = MARGIN_LEFT + rand.nextInt((MARGIN_RIGHT - MARGIN_LEFT) / SIZE) * SIZE;
-            this.y = MARGIN_TOP + rand.nextInt((MARGIN_BOTTOM - MARGIN_TOP) / SIZE) * SIZE;
-            this.color = Color.RED;
-        }
-
-        public void draw(Graphics g) {
-            g.setColor(color);
-            g.fillOval(x, y, SIZE, SIZE);
-        }
-
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, SIZE, SIZE);
+    }
 }
---
+++ snakehilos/src/main/java/co/edu/uniquindio/poo/model/GamePanel.java
@@ -0,0 +1,127 @@
+package co.edu.uniquindio.poo.model;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class GamePanel extends JPanel {
+    List<Vivora> vivoras;
+    CopyOnWriteArrayList<Food> foods = new CopyOnWriteArrayList<>();
+    protected static final int MARGIN_LEFT = 20;
+    protected static final int MARGIN_RIGHT = 780;
+    protected static final int MARGIN_TOP = 20;
+    protected static final int MARGIN_BOTTOM = 580;
+
+    public GamePanel(List<Vivora> vivoras) {
+        this.vivoras = vivoras;
+        setLayout(null); // Use absolute positioning for the game area
+        setBackground(Color.WHITE);
+        setFocusable(true);
+        setPreferredSize(new Dimension(MARGIN_RIGHT - MARGIN_LEFT, MARGIN_BOTTOM - MARGIN_TOP));
+
+        // Add key listener for controlling the active snake
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                for (Vivora vivora : vivoras) {
+                    if (vivora.controladaPorUsuario) {
+                        switch (e.getKeyCode()) {
+                            case KeyEvent.VK_UP:
+                                if (!vivora.getDireccion().equals("abajo"))
+                                    vivora.setDireccion("arriba");
+                                break;
+                            case KeyEvent.VK_DOWN:
+                                if (!vivora.getDireccion().equals("arriba"))
+                                    vivora.setDireccion("abajo");
+                                break;
+                            case KeyEvent.VK_LEFT:
+                                if (!vivora.getDireccion().equals("derecha"))
+                                    vivora.setDireccion("izquierda");
+                                break;
+                            case KeyEvent.VK_RIGHT:
+                                if (!vivora.getDireccion().equals("izquierda"))
+                                    vivora.setDireccion("derecha");
+                                break;
+                        }
+                    }
+                }
+            }
+        });
+
+        // Generate initial food
+        generarComida();
+    }
+
+    public void generarComida() {
+        foods.add(new Food());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draw margins
+        g.setColor(Color.BLACK);
+        g.drawRect(MARGIN_LEFT, MARGIN_TOP, MARGIN_RIGHT - MARGIN_LEFT, MARGIN_BOTTOM - MARGIN_TOP);
+
+        for (Vivora vivora : vivoras) {
+            if (vivora.viva) {
+                vivora.draw(g);
+            }
+        }
+        for (Food food : foods) {
+            food.draw(g);
+        }
+    }
+
+    // Method to update the game state
+    public void actualizarJuego() {
+        for (Vivora vivora : vivoras) {
+            if (!vivora.viva) {
+                continue;
+            }
+
+            // Move the snake
+            vivora.moverSnake();
+
+            // Check for self-collision
+            Nodo cabeza = vivora.cabeza;
+            Nodo current = vivora.cabeza.siguienteNodo;
+            while (current != null) {
+                if (cabeza.x == current.x && cabeza.y == current.y) {
+                    vivora.viva = false;
+                    break;
+                }
+                current = current.siguienteNodo;
+            }
+
+            // Check for food collisions
+            for (Food food : foods) {
+                if (cabeza.x == food.x && cabeza.y == food.y) {
+                    vivora.agregarNodo(); // Add a new node to the snake
+                    foods.remove(food); // Remove the food
+                    generarComida(); // Generate new food
+                }
+            }
+
+            // Check for collisions with other snakes
+            for (Vivora otherVivora : vivoras) {
+                if (otherVivora != vivora && otherVivora.viva) {
+                    Nodo otherCurrent = otherVivora.cabeza;
+                    while (otherCurrent != null) {
+                        if (cabeza.x == otherCurrent.x && cabeza.y == otherCurrent.y) {
+                            vivora.viva = false;
+                            break;
+                        }
+                        otherCurrent = otherCurrent.siguienteNodo;
+                    }
+                }
+            }
+        }
+
+        repaint();
+    }
+}
